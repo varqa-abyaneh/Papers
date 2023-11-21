07ec4463defee03e8eeeb8b70fbadbe7f969eea0
Varqa Abyaneh <varqa.abyaneh@opetek.io>
2023-11-21 12:54:47 +0000

    saving prior to cleaning

--- Paper_1/main.py
+++ Paper_1/main.py
@@ -15,7 +15,6 @@ from matplotlib.ticker import FuncFormatter
 from mpl_toolkits.mplot3d import Axes3D
 from scipy import sparse
 from scipy.sparse.linalg import eigs, eigsh
-
 import quantum_functions as qf
 
 plt.style.use(["science", "notebook"])
@@ -28,18 +27,18 @@ logger = structlog.get_logger()
 # Setup
 ########
 
-
 # Universal constants
 hbar = 1.05457182e-34
 
-# Mass and charge
+# Mass and charge. Both ions are protons 
 m_1 = 1.67262192e-27  # 1st particle mass (kg)
 m_2 = 1.67262192e-27  # 2st particle mass (kg)
 q_1 = 1.60217663e-19  # 1st particle charge (C)
 q_2 = 1.60217663e-19  # 2st particle charge (C)
 
-# Coulomb regularisation
-epsilon = 1e-15  # Regularisation to stop potential energy singularity
+# Numerical parameters
+epsilon = 1e-15  # Regularisation to stop potential energy singularity (m)
+Hard_Wall = 0 # Represents infinite potential where F_Total > F_Max
 
 # Initial confinement
 L = 1.00e-6
@@ -47,10 +46,10 @@ d = 1.00e-12  # QZE confinement region (m)
 N = 30 # Number PDE spatial grid points in x and y dimension
 delta_X = d / N  # Spatial grid length (m)
 distance_threshold = 1e-15  # fusion threshold distance (m)
-Hard_Wall = 0
 
-# Extended confinement
-confinement_ratio = 2
+
+# Extended confinement. Required for calculating leakage
+confinement_ratio = 2 
 d_ext = confinement_ratio * d
 N_ext = confinement_ratio * N
 delta_X_ext = d_ext / N_ext
@@ -69,7 +68,7 @@ deltaT = 5e-15  # Time step (currently 1e-20 seconds seems to be scale to evolve
 ########
 
 
-# Create confined Hamiltonian H_L
+# Create confined Hamiltonian H_d
 V = qf.coulomb_potential(X, Y, q_1, q_2, epsilon) + qf.boundary_potential(
     X, Y, q_1, q_2, d, L, Hard_Wall
 )
@@ -94,12 +93,6 @@ eigenvector_2D = qf.eigenvector_1D_to_2D(eigenvector, N)
 # Calculate the probability particles are within 1e-15m of one another
 prob = qf.probability_within_distance(eigenvector_2D, delta_X, distance_threshold)
 print(f"The probability of |x1 - x2| < {distance_threshold} m is {prob}")
-
-# # Create relative momementum operator
-# p_rel=ql.create_relative_momentum_matrix(N, delta_X)
-
-# expectation_value = np.dot(np.conj(eigenvector), np.dot(p_rel.toarray(), eigenvector))
-# print("Expectation value:", expectation_value)
 
 
 #######
@@ -144,17 +137,7 @@ psi_1D_t, psi_2D_t = qf.solve_future_states(
 )
 
 # Calculate leakage
-# leakage = ql.calculate_leakage(psi_2D_t[1], N, N_ext)
 leakage = qf.calculate_leakage(X, Y, q_1, q_2, d, L, psi_2D_t, N, N_ext)
-
-
-# # Create extended relative momementum time vector
-# p_rel_ext=ql.create_relative_momentum_matrix(N_ext, delta_X_ext)
-# relative_expected_momentum_t = ql.solve_future_expected_relative_momentum(num_time_steps+1, psi_1D_t, p_rel_ext)
-
-# New line by VA for squared raltive momentum
-# relative_expected_momentum_squared_t = ql.solve_future_expected_relative_momentum_squared(num_time_steps + 1, psi_1D_t, p_rel_ext)
-
 
 
 # ######
@@ -162,7 +145,7 @@ leakage = qf.calculate_leakage(X, Y, q_1, q_2, d, L, psi_2D_t, N, N_ext)
 # ######
 
 # Manual graphical update 2D wave function
-qf.graphic_manual_2D_evolve(num_time_steps, psi_2D_t, X_ext, Y_ext, deltaT)
+# qf.graphic_manual_2D_evolve(num_time_steps, psi_2D_t, X_ext, Y_ext, deltaT)
 
 # 2D animation
 # ql.graphic_animation_2D(num_time_steps, psi_2D_t, deltaT, frames_per_second=2, filename = '2D_10e-12.gif')
@@ -173,17 +156,28 @@ qf.graphic_manual_2D_evolve(num_time_steps, psi_2D_t, X_ext, Y_ext, deltaT)
 # 1D animation
 # ql.graphic_animation_1D(num_time_steps, psi_2D_t, deltaT, frames_per_second=2, filename = '1D_10e-12.gif')
 
-# # Plot eigenvector on initial confinement
-# formatter = FuncFormatter(ql.format_ticks)
-# plt.figure(figsize=(8, 8))
-# plt.pcolormesh(X, Y, eigenvector_2D**2, cmap="nipy_spectral")
-# plt.xlabel("Particle 1 Position (m)")
-# plt.ylabel("Particle 2 Position (m)")
-# plt.axis("on")
-# plt.title(f"Ground State: deuterium-tritium QZE trapped in {d:.1e}m region")
-# plt.gca().xaxis.set_major_formatter(formatter)
-# plt.gca().yaxis.set_major_formatter(formatter)
-# plt.show()
+# Plot eigenvector on initial confinement
+formatter = FuncFormatter(qf.format_ticks)
+plt.figure(figsize=(8, 8))
+plt.pcolormesh(X, Y, eigenvector_2D**2, cmap="nipy_spectral")
+plt.xlabel("Particle 1 Position (m)")
+plt.ylabel("Particle 2 Position (m)")
+plt.axis("on")
+plt.title(f"Ground State: deuterium-tritium QZE trapped in {d:.1e}m region")
+plt.gca().xaxis.set_major_formatter(formatter)
+plt.gca().yaxis.set_major_formatter(formatter)
+plt.show()
+# Flatten the X, Y, and squared eigenvector arrays for saving
+X_flat = X.flatten()
+Y_flat = Y.flatten()
+eigenvector_2D_flat = (eigenvector_2D**2).flatten()
+# Combine the flattened arrays
+combined_array = np.column_stack((X_flat, Y_flat, eigenvector_2D_flat))
+# Save to CSV
+np.savetxt("/home/varqa/schrodinger/2d_wavefunction_data.csv", combined_array, delimiter=",", header="Particle 1 Position,Particle 2 Position,Eigenvector Squared", comments='')
+# Print message to confirm saving
+print("Data saved to /home/varqa/schrodinger/2d_wavefunction_data.csv")
+
 
 # # Plot same eigenvector on extended grid
 # plt.figure(figsize=(8,8))
@@ -191,24 +185,28 @@ qf.graphic_manual_2D_evolve(num_time_steps, psi_2D_t, X_ext, Y_ext, deltaT)
 # plt.axis('on')
 # plt.show()
 
-# # Plot relative_expected_momentum against time
-# # Create a time array
-# time_array = np.arange(0, (num_time_steps+1) * deltaT, deltaT)
-# formatter = FuncFormatter(ql.format_ticks)
-# plt.figure(figsize=(8,8))
-# plt.plot(time_array, relative_expected_momentum_t)
-# plt.title(f"Relative expected momentum against time")
-# plt.xlabel("Time (s)")  # Assuming delta_T is in seconds
-# plt.ylabel("Relative expected momentum (kg m/s)")
-# plt.gca().xaxis.set_major_formatter(formatter)
-# plt.gca().yaxis.set_major_formatter(formatter)
-# plt.show()
-# # Combine the time_array and relative_expected_momentum_t for saving
-# time_array_real = np.real(time_array)
-# relative_expected_momentum_t_real = np.real(relative_expected_momentum_t)
-# combined_array = np.column_stack((time_array_real, relative_expected_momentum_t_real))
-# # Save to CSV
-# np.savetxt("relative_expected_momentum_vs_time.csv", combined_array, delimiter=",", header="Time,Relative Expected Momentum", comments='')
+
+# Plot relative_expected_momentum against time
+# Create a time array
+time_array = np.arange(0, (num_time_steps+1) * deltaT, deltaT)
+formatter = FuncFormatter(qf.format_ticks)
+plt.figure(figsize=(8,8))
+
+
+
+plt.plot(time_array, relative_expected_momentum_t)
+plt.title(f"Relative expected momentum against time")
+plt.xlabel("Time (s)")  # Assuming delta_T is in seconds
+plt.ylabel("Relative expected momentum (kg m/s)")
+plt.gca().xaxis.set_major_formatter(formatter)
+plt.gca().yaxis.set_major_formatter(formatter)
+plt.show()
+# Combine the time_array and relative_expected_momentum_t for saving
+time_array_real = np.real(time_array)
+relative_expected_momentum_t_real = np.real(relative_expected_momentum_t)
+combined_array = np.column_stack((time_array_real, relative_expected_momentum_t_real))
+# Save to CSV
+np.savetxt("relative_expected_momentum_vs_time.csv", combined_array, delimiter=",", header="Time,Relative Expected Momentum", comments='')
 
 # # Plot relative_expected_momentum squared against time
 # # Create a time array
